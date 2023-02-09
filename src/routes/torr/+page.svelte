8a3b9e778a67ca4186ab1eff21e84d79c337b6a5
Tanmay Sharma <58450007+Tanmay8043@users.noreply.github.com>
2023-02-09 12:10:08 +0530

    S

--- src/routes/torr/+page.svelte
+++ src/routes/torr/+page.svelte
@@ -7,6 +7,9 @@
     const WebTorrent = wt.default;
     client = new WebTorrent();
 
+    if (!WebTorrent.WEBRTC_SUPPORT) {
+      console.error('This browser is unsupported. Please use a browser with WebRTC support.')
+    }
     client.on('error', err => {
       console.error('ERROR: ' + err.message)
     })
@@ -32,11 +35,6 @@
   var el;
   var progress = 0;
   const download=()=>{
-    //this is file
-    // const magnet="magnet:?xt=urn:btih:018eac9c3745971ae770017098dbcba892de2b8f&dn=Manthan+and+Kajal.mp4&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com"
-    //this is folder
-    // const magnet = "magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
-    // const magnet = "magnet:?xt=urn:btih:b395ddc7b1a40610fba16f13dfca9e95f299be3a&dn=ISArcExtract&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com"
     client.add(magnet, onTorrent)
   }
 
@@ -117,7 +115,7 @@
   <p>Download files-</p>
   <div bind:this={dataEL}>
     {#each links as link, i}
-    <a download="{link.name}" target="_blank" rel="noreferrer" href={link.downloadURL}> {link.name} </a>
+    <a download="{link.name}" target="_blank" rel="noreferrer" href={link.downloadURL}> {i+1}. {link.name} </a>
     <br/>
     {/each}
   </div>
